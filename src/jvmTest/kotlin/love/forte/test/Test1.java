b28905c91844b7eea9e619934dd440d469c7b504
ForteScarlet <ForteScarlet@163.com>
2020-12-31 22:11:44 +0800

    test and remove some

--- src/jvmTest/kotlin/love/forte/test/Test1.java
+++ src/jvmTest/kotlin/love/forte/test/Test1.java
@@ -1,13 +1,20 @@
-package love.forte.love.forte.test;
+package love.forte.test;
 
-import catcode.CatCodeUtil;
+import catcode.WildcatCodeUtil;
 
 /**
  * @author ForteScarlet
  */
 public class Test1 {
     public static void main(String[] args) {
-        CatCodeUtil.INSTANCE.toCat("");
+        // 获取CQ工具类。
+        WildcatCodeUtil cqCodeUtil = WildcatCodeUtil.getInstance("CQ");
+
+        // 构建一个参数为 code=123的at类型的CQ码
+        String cqCode = cqCodeUtil.toCat("at", "code=123");
+
+        // [CQ:at,code=123]
+        System.out.println(cqCode);
 
 
     }
